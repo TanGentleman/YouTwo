accdba39f6267d14259c271379d51e19b9f36c7d
Tanuj <Tanuj@cmu.edu>
2025-06-10 12:23:37 -0700

    Update modal_app.py

--- src/yt_modal/modal_app.py
+++ src/yt_modal/modal_app.py
@@ -5,7 +5,7 @@ import sys
 
 # Current directory path
 src_dir = Path(__file__).parent.parent
-modal_dir = src_dir / "yt_modal"
+agent_dir = src_dir / "yt_agent"
 gradio_dir = src_dir / "yt_gradio"
 rag_dir = src_dir / "yt_rag"
 
@@ -15,12 +15,12 @@ web_image = modal.Image.debian_slim(python_version="3.10").pip_install(
     "fastapi[standard]==0.115.4",
     "gradio[mcp]==5.33.0",
     "requests",
+    "smolagents",
     # Add any other dependencies you need
 ).add_local_file(rag_dir / "rag.py", "/root/src/yt_rag/rag.py") \
 .add_local_file(gradio_dir / "app.py", "/root/src/yt_gradio/app.py") \
+.add_local_file(agent_dir / "agent.py", "/root/src/yt_agent/agent.py") \
 .add_local_file(src_dir / "schemas.py", "/root/src/schemas.py")
-
-
 
 app = modal.App("youtwo-gradio", image=web_image)
 
@@ -35,7 +35,8 @@ volume = modal.Volume.from_name("youtwo-volume", create_if_missing=True)
 @app.function(
     volumes={"/data": volume},
     secrets=[
-        modal.Secret.from_name("vectara")
+        modal.Secret.from_name("vectara"),
+        modal.Secret.from_name("nebius")
     ],
     max_containers=1,
     scaledown_window=TIME_LIMIT,  # 3540 seconds, within the allowed 2-3600 second range
